7b272659706aab144adcf210ae213874248184b2
Lezcano Tech  <olivertwist200054@gmail.com>
2024-05-06 07:07:27 +0100

    updated

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,40 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj: login;
 
+  constructor(private http: HttpClient, private router: Router) {
+    this.loginObj = new login();
+  }
+
+  onLogin() {
+    debugger;
+    this.http.post('https://freeapi.miniprojectideas.com/api/User/Login', this.loginObj).subscribe((res:any)=>{
+      if(res.result){
+        alert("Login Success");
+        this.router.navigateByUrl('/dashboard')
+      }else{
+        alert(res.message)
+      }
+    })
+  }
+}
+
+export class login{
+  EmailId: string;
+  Password: string;
+  constructor () {
+    this.EmailId = "";
+    this.Password = "";
+  }
 }
